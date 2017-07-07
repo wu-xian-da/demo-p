1f8b39932f555444b6eb1dd1e9ed147159709b95
wu.changchun <wu.changchun@jianfeitech.com>
2017-07-07 16:12:28 +0800

    添加cdnDomain

--- demo-w/src/main/java/com/jianfei/d/base/BaseController.java
+++ demo-w/src/main/java/com/jianfei/d/base/BaseController.java
@@ -7,8 +7,25 @@ package com.jianfei.d.base;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.jianfei.d.config.GlobalSiteConfig;
+import com.jianfei.d.config.SiteConfig;
 
 public class BaseController {
 
 	protected Logger log = LoggerFactory.getLogger(getClass());
+	
+	@Autowired
+	protected SiteConfig siteConfig;
+	
+	/***
+	 * cdnDomain
+	 * @return
+	 */
+	@ModelAttribute(GlobalSiteConfig.CND_DOMAIN)
+	public String getCdnDomain(){
+		return siteConfig.getCdnDomain();
+	}
 }
---
+++ demo-w/src/main/java/com/jianfei/d/config/GlobalSiteConfig.java
@@ -0,0 +1,16 @@
+/**
+  *project demo-w
+  *@author changchun.wu
+  *2017年7月7日下午4:05:32
+  */
+package com.jianfei.d.config;
+
+/***
+ * 站点全局配置信息
+ * @author changchun.wu
+ */
+public class GlobalSiteConfig {
+	
+	//静态服务器资源域名cdnDomain
+	public static final String CND_DOMAIN = "cdnDomain";
+}
