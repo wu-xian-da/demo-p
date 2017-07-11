e9c261663fc5a15e9f0ff861c6d79e23d5e73060
wu.changchun <wu.changchun@jianfeitech.com>
2017-07-11 17:33:44 +0800

    HttpClientUtils

--- demo-l/src/main/java/com/jianfei/d/common/utils/HttpClientUtils.java
+++ demo-l/src/main/java/com/jianfei/d/common/utils/HttpClientUtils.java
@@ -6,9 +6,12 @@
 package com.jianfei.d.common.utils;
 
 import java.io.IOException;
+import java.io.InterruptedIOException;
 import java.io.UnsupportedEncodingException;
+import java.net.UnknownHostException;
 import java.util.List;
 
+import javax.net.ssl.SSLException;
 import javax.net.ssl.SSLHandshakeException;
 
 import org.apache.http.Consts;
@@ -21,17 +24,29 @@ import org.apache.http.NameValuePair;
 import org.apache.http.NoHttpResponseException;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpRequestRetryHandler;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.params.ClientPNames;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.ConnectTimeoutException;
 import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.LayeredConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingClientConnectionManager;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.CoreConnectionPNames;
 import org.apache.http.params.HttpParams;
@@ -43,84 +58,86 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HttpClientUtils {
-	private static final Logger logger = LoggerFactory
-			.getLogger(HttpClientUtils.class);
+	private static final Logger logger = LoggerFactory.getLogger(HttpClientUtils.class);
 	
 	
 	//重试次数
 	private static final int retryTime = 3;
 	
-	// 设置连接超时时间
-	private static int REQUEST_TIMEOUT = 5 * 1000; // 设置请求超时5秒钟
-	private static int SO_TIMEOUT = 3 * 1000; // 设置等待数据超时时间3秒钟
-	private static Long CONN_MANAGER_TIMEOUT = 500L; // 连接不够用的时候等待超时时间
+	// 请求的超时设置
+	private static final int connectionRequestTimeout = 3000;
+	private static final int connectTimeout = 1000;
+	private static final int socketTimeout = 2000;
 			
-	//线程安全DefaultHttpClient
-	private static DefaultHttpClient httpClient;
+	private static CloseableHttpClient httpClient;
+	private static RequestConfig requestConfig;
 	
-
 	static {
-		// 设置组件参数, HTTP协议的版本,1.1/1.0/0.9
-		HttpParams params = new BasicHttpParams();
-		HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
-		HttpProtocolParams.setUserAgent(params, "HttpComponents/1.1");
-		HttpProtocolParams.setUseExpectContinue(params, true);
-		
-		params.setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT,REQUEST_TIMEOUT);
-		params.setParameter(CoreConnectionPNames.SO_TIMEOUT, SO_TIMEOUT);
-		params.setParameter(ClientPNames.CONN_MANAGER_TIMEOUT,CONN_MANAGER_TIMEOUT);
-
-		// 设置访问协议
-		SchemeRegistry schreg = new SchemeRegistry();
-		schreg.register(new Scheme("http", 80, PlainSocketFactory.getSocketFactory()));
-		schreg.register(new Scheme("https", 443, SSLSocketFactory.getSocketFactory()));
-
-		// 多连接的线程安全的管理器
-		PoolingClientConnectionManager pccm = new PoolingClientConnectionManager(
-				schreg);
-		pccm.setDefaultMaxPerRoute(50); // 每个主机的最大并行链接数
-		pccm.setMaxTotal(200); // 客户端总并行链接最大数
-
-		httpClient = new DefaultHttpClient(pccm, params);
-
-		
-		// 可以自动重连
-		HttpRequestRetryHandler requestRetryHandler2 = new HttpRequestRetryHandler() {
-			// 自定义的恢复策略
-			public synchronized boolean retryRequest(IOException exception,
-					int executionCount, HttpContext context) {
-				// 设置恢复策略，在发生异常时候将自动重试3次
-				if (executionCount > retryTime) {
-					// 超过最大次数则不需要重试
-					return false;
-				} 
-				if (exception instanceof NoHttpResponseException) {
-					// 服务停掉则重新尝试连接
-					return true;
-				}
-				if (exception instanceof SSLHandshakeException) {
-					// SSL异常不需要重试
-					return false;
-				}
-				
-				HttpRequest request = (HttpRequest) context.getAttribute(ExecutionContext.HTTP_REQUEST);
-				boolean idempotent = (request instanceof HttpEntityEnclosingRequest);
-				if (!idempotent) {
-					// 请求内容相同则重试
-					return true;
-				}
-				return false;
-			}
-		};
-		
-		
-		httpClient.setHttpRequestRetryHandler(requestRetryHandler2);
+		ConnectionSocketFactory plainsf = PlainConnectionSocketFactory.getSocketFactory();
+        LayeredConnectionSocketFactory sslsf = SSLConnectionSocketFactory.getSocketFactory();
+        Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", plainsf)
+                .register("https", sslsf)
+                .build();
+        
+        PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(registry);
+        // 将最大连接数增加到200
+        cm.setMaxTotal(200);
+        // 将每个路由基础的连接增加到20
+        cm.setDefaultMaxPerRoute(20);
+        
+        //请求重试处理
+        HttpRequestRetryHandler httpRequestRetryHandler = new HttpRequestRetryHandler() {
+            public boolean retryRequest(IOException exception,int executionCount, HttpContext context) {
+                if (executionCount >= retryTime) {// 如果已经重试了3次，就放弃                    
+                    return false;
+                }
+                if (exception instanceof NoHttpResponseException) {// 如果服务器丢掉了连接，那么就重试                    
+                    return true;
+                }
+                if (exception instanceof SSLHandshakeException) {// 不要重试SSL握手异常                    
+                    return false;
+                }                
+                if (exception instanceof InterruptedIOException) {// 超时                    
+                    return false;
+                }
+                if (exception instanceof UnknownHostException) {// 目标服务器不可达                    
+                    return false;
+                }
+                if (exception instanceof ConnectTimeoutException) {// 连接被拒绝                    
+                    return false;
+                }
+                if (exception instanceof SSLException) {// ssl握手异常                    
+                    return false;
+                }
+                
+                HttpClientContext clientContext = HttpClientContext.adapt(context);
+                HttpRequest request = clientContext.getRequest();
+                // 如果请求是幂等的，就再次尝试
+                if (!(request instanceof HttpEntityEnclosingRequest)) {                    
+                    return true;
+                }
+                return false;
+            }
+        };
+        
+        requestConfig = RequestConfig.custom()  
+                .setConnectionRequestTimeout(connectionRequestTimeout)  
+                .setConnectTimeout(connectTimeout)  
+                .setSocketTimeout(socketTimeout)  
+                .build();
+        
+       httpClient = HttpClients.custom()
+                    .setConnectionManager(cm)
+                    .setRetryHandler(httpRequestRetryHandler)
+                    .build();
 	}
 
 	// Post
 	public static String post(String url, List<NameValuePair> params) {
 		String result = null;
 		HttpPost post = new HttpPost(url);
+		post.setConfig(requestConfig);
 		HttpEntity entityRos = null;
 		try {
 			// 设置参数
@@ -149,6 +166,7 @@ public class HttpClientUtils {
 	public static String get(String url) {
 		String result = null;
 		HttpGet get = new HttpGet(url);
+		get.setConfig(requestConfig);
 		HttpEntity entityRos = null;
 		try {
 			// 发送请求
@@ -175,6 +193,7 @@ public class HttpClientUtils {
 	public static String post(String url,  String jsonStr) {
 		String result = null;
 		HttpPost post = new HttpPost(url);
+		post.setConfig(requestConfig);
 		HttpEntity entityRos = null;
 		try {
 			StringEntity s = new StringEntity(jsonStr,"UTF-8");  
